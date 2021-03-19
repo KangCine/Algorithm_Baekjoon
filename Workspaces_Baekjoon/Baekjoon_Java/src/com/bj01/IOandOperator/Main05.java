6234866a69f1aaab7cfb9653f500eaa217366661
강원기(KangWonKi) <78646944+KangCine@users.noreply.github.com>
2021-03-20 00:20:40 +0900

    NO.1000

---
+++ Workspaces_Baekjoon/Baekjoon_Java/src/com/bj01/IOandOperator/Main05.java
@@ -0,0 +1,17 @@
+//No.1000
+package com.bj01.IOandOperator;
+
+import java.util.Scanner;
+
+public class Main05 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		
+		System.out.println(num1 + num2);
+		sc.close();
+		
+	}
+}
