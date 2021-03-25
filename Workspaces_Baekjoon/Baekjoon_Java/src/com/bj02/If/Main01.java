ded5b6a0961f50f4fbc0269a9ef9217fb9ea7053
강원기(KangWonKi) <78646944+KangCine@users.noreply.github.com>
2021-03-25 20:20:12 +0900

    NO.1330

---
+++ Workspaces_Baekjoon/Baekjoon_Java/src/com/bj02/If/Main01.java
@@ -0,0 +1,22 @@
+//No.1330
+package com.bj02.If;
+
+import java.util.Scanner;
+
+public class Main01 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		if(A > B) {
+			System.out.println(">");
+		}else if(A < B) {
+			System.out.println("<");
+		}else if(A == B) {
+			System.out.println("==");
+		}
+		sc.close();
+	}
+}
