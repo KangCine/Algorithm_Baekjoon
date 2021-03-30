1c042cddcafef3fbb4c3ebf7d61ec40496bf739c
강원기(KangWonKi) <78646944+KangCine@users.noreply.github.com>
2021-03-30 20:08:12 +0900

    NO.2739

---
+++ Workspaces_Baekjoon/Baekjoon_Java/src/com/bj03/For/Main01.java
@@ -0,0 +1,19 @@
+//No.2739
+package com.bj03.For;
+
+import java.util.Scanner;
+
+public class Main01 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int dan = sc.nextInt();
+		
+		for(int i = 1; i <= 9; i++) {
+			System.out.println(dan + " * " + i + " = " + dan*i );
+		}
+		
+		sc.close();
+	}
+}
