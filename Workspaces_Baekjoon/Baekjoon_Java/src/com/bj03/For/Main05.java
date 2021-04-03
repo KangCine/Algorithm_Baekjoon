6f7617ca8f5c2b11196a8effc25de3e235d5f55f
강원기(KangWonKi) <78646944+KangCine@users.noreply.github.com>
2021-04-04 03:56:28 +0900

    NO.2741

---
+++ Workspaces_Baekjoon/Baekjoon_Java/src/com/bj03/For/Main05.java
@@ -0,0 +1,25 @@
+//No.2741
+package com.bj03.For;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main05 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int line = Integer.parseInt(br.readLine());
+		
+		for(int i = 1; i <= line; i++) {
+			bw.write(i + "\n");
+		}
+		bw.close();
+		br.close();
+	}
+}
