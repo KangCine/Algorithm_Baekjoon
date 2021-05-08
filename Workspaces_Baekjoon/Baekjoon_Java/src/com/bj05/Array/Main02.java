bdf0bed4cce562b2c01cd352f64b0861ab1f6d5e
KangCine <kwg940930@gmail.com>
2021-05-09 00:57:42 +0900

    NO2562 최댓값

---
+++ Workspaces_Baekjoon/Baekjoon_Java/src/com/bj05/Array/Main02.java
@@ -0,0 +1,28 @@
+//No.2562
+package com.bj05.Array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main02 {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int[] num = new int [9];
+		int[] tmp = new int [9];
+		
+		for(int i = 0; i < num.length; i++) {
+			num[i] = sc.nextInt();
+			tmp[i] = num[i];
+		}
+		Arrays.sort(num);
+		System.out.println(num[8]);
+		for(int i=0; i < num.length; i++) {
+			if(tmp[i] == num[8]) {
+				System.out.println(i+1);
+			}
+		}
+
+	}
+}
