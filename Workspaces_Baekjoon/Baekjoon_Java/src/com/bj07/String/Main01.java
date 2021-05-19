c011b9ac57c161d9c45a7cefe248811d8910c893
KangCine <kwg940930@gmail.com>
2021-05-19 12:39:26 +0900

    NO.11654 아스키 코드

---
+++ Workspaces_Baekjoon/Baekjoon_Java/src/com/bj07/String/Main01.java
@@ -0,0 +1,18 @@
+//NO.11654
+package com.bj07.String;
+
+import java.util.Scanner;
+
+public class Main01 {
+
+	public static void main(String[] args) {
+			
+		Scanner sc = new Scanner(System.in);
+			
+		int inner = sc.next().charAt(0);
+			
+		System.out.println(inner);
+
+	}
+
+}
