0718eb1d5231b2a15f9ca7617654e7ad5e980515
KangCine <kwg940930@gmail.com>
2021-05-23 17:38:25 +0900

    NO.11720 숫자의 합

---
+++ Workspaces_Baekjoon/Baekjoon_Java/src/com/bj07/String/Main02.java
@@ -0,0 +1,26 @@
+//NO.11720
+package com.bj07.String;
+
+import java.util.Scanner;
+
+public class Main02 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		String num = sc.next();
+		
+		int sum = 0;
+		
+		for(int i = 0; i < n; i++) {
+			sum += num.charAt(i)-'0';
+		}
+		
+		System.out.println(sum);
+		
+		sc.close();
+	}
+
+}
